729e975b36431e541ff251659139494706735f72
Sarah Sester <sarahsester11@gmail.com>
2020-11-09 13:28:02 +0100

    Added dataframe in app.py. Table in streets.html is now showing values of dataframe.

--- App/app.py
+++ App/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+import pandas as pd
+import numpy as np
+
 
 # set variables in terminal:
 # set FLASK_APP=app.py
 # set FLASK_ENV=development
 
-# to start the app:
-# - activate virtual environment: "venv\Scripts\activate.bat"
-# - run "python app.py" or "flask run" in your terminal
+# to start the app, run "python app.py" or "flask run" in your terminal
 
 app = Flask(__name__)
 Bootstrap(app)
+
+
+df = pd.DataFrame({'Street Name': ["Schillerpromenade", "Goethestraße"],
+                   "City": ["Berlin", "Chemnitz"],
+                   "Person": ["Friedrich Schiller", "Johann Wolfgang von Goethe"]})
 
 
 @app.route("/")
@@ -22,9 +28,13 @@ def index():
 def index_post():
     latitude = request.form['latitude']
     longitude = request.form['longitude']
-    #processed_text = coordinate1.upper()
-    #return processed_text
-    return render_template('streets.html')
+    return render_template('streets.html',
+                           latitude=latitude,
+                           longitude=longitude,
+                           column_names=df.columns.values,
+                           row_data=list(df.values.tolist()),
+                           link_column="Person",
+                           zip=zip)
 
 
 @app.route("/streets")
