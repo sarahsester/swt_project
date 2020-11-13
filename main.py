eb7d47b0171e477ee555c1a28f990ec1f87ef6cc
Sarah Sester <sarahsester11@gmail.com>
2020-11-13 20:23:37 +0100

    Included SPARQL query and map.

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+from SPARQL_queries import create_df_level1, create_map_level1
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# set variables in terminal:
+# set FLASK_APP=app.py
+# set FLASK_ENV=development
+
+# to start the app, run "python main.py" or "flask run" in your terminal
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+app = Flask(__name__)
+Bootstrap(app)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route('/', methods=['POST'])
+def index_post():
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    radius = request.form['radius']
+    df = create_df_level1(latitude=latitude, longitude=longitude, radius=radius, limit='30')
+    _map = create_map_level1(latitude=latitude, longitude=longitude, radius=radius, limit='30')
+    return render_template('level1.html',
+                           latitude=latitude,
+                           longitude=longitude,
+                           radius=radius,
+                           column_names=df.columns.values,
+                           row_data=list(df.values.tolist()),
+                           link_column="somebodyLabel",
+                           zip=zip,
+                           _map=_map)
+
+
+@app.route("/level1")
+def level1():
+    return render_template('level1.html')
+
+
+@app.route("/level2")
+def level2():
+    return render_template('level2.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
