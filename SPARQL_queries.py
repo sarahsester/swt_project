9728855c090af1a595850046af6e222a8c123a40
Sarah Sester <sarahsester11@gmail.com>
2020-11-26 19:01:29 +0100

    Added second level.

--- SPARQL_queries.py
+++ SPARQL_queries.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
+import haversine as hs
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 
 def create_df_level1(latitude='49.487777777', longitude='8.466111111', radius='15', limit='30'):
-    endpoint = 'https://query.wikidata.org/sparql'
     query = '''
         prefix bd:       <http://www.bigdata.com/rdf#>
         prefix geo:      <http://www.opengis.net/ont/geosparql#>
@@ -12,7 +13,8 @@ def create_df_level1(latitude='49.487777777', longitude='8.466111111', radius='1
         prefix wikibase: <http://wikiba.se/ontology#>
 
 
-        SELECT ?xLabel ?x ?somebodyLabel ?somebody ?classLabel ?otherLocation ?dist
+        SELECT ?xLabel ?x ?somebodyLabel ?somebody ?otherLocation 
+               (GROUP_CONCAT(?classLabel; separator=', ') AS ?classdescription)
         WHERE
         {{
           BIND('Point({longitude} {latitude})'^^geo:wktLiteral AS ?currentLocation).
@@ -23,29 +25,27 @@ def create_df_level1(latitude='49.487777777', longitude='8.466111111', radius='1
           }}
           # x is named after
           ?x wdt:P138 ?somebody .
+          
+          # that somebody is a human
+          ?somebody wdt:P31 wd:Q5 .
 
           # which Class is x (e.g. Street, church)
           ?x wdt:P31  ?class . 
-
-          # x is a Street
-          #?x wdt:P31 wd:Q79007 .
-
-          # hat Staat Deutschland
-          # ?x wdt:P17  ?Q183 .
-
+          
+          # the class has a german label 
+          ?class rdfs:label ?classLabel .
+          FILTER( lang(?classLabel) = "de" )
+          
           # Retrieve Labels
           SERVICE wikibase:label {{ bd:serviceParam wikibase:language "de". }} 
-
-          # Compute Distance
-          # BIND(geof:distance(?otherLocation, ?currentLocation) as ?dist) 
+          
         }} 
-
+        GROUP BY ?xLabel ?x ?somebodyLabel ?somebody ?otherLocation
         LIMIT {limit}
-        #ORDER BY ?dist
-
         '''.format(longitude=longitude, latitude=latitude, radius=radius, limit=limit)
 
     # Initializing SPARQL Wrapper and querying
+    endpoint = 'https://query.wikidata.org/sparql'
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
@@ -55,11 +55,132 @@ def create_df_level1(latitude='49.487777777', longitude='8.466111111', radius='1
     for col in df.columns:
         df[col] = df[col].apply(lambda x: x['value'])
 
+    # Compute distance and sort according to distance
+    df['distance'] = df['otherLocation'].apply(lambda point: compute_distance(new_point=point,
+                                                                              current_longitude=longitude,
+                                                                              current_latitude=latitude))
+    df = df.sort_values(by=['distance'])
+
     return df
 
 
 def create_map_level1(latitude='49.487777777', longitude='8.466111111', radius='15', limit='30'):
-    map = '''https://query.wikidata.org/embed.html#%23defaultView%3AMap%0A%20%20%20%20%20%20%20%20prefix%20bd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.bigdata.com%2Frdf%23%3E%0A%20%20%20%20%20%20%20%20prefix%20geo%3A%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0A%20%20%20%20%20%20%20%20prefix%20wd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0A%20%20%20%20%20%20%20%20prefix%20wdt%3A%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F%3E%0A%20%20%20%20%20%20%20%20prefix%20wikibase%3A%20%3Chttp%3A%2F%2Fwikiba.se%2Fontology%23%3E%0A%0A%0A%20%20%20%20%20%20%20%20SELECT%20%3FxLabel%20%3Fx%20%3FsomebodyLabel%20%3Fsomebody%20%3FclassLabel%20%3FotherLocation%20%3Fdist%0A%20%20%20%20%20%20%20%20WHERE%0A%20%20%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20%20BIND('Point({longitude}%20{latitude})'%5E%5Egeo%3AwktLiteral%20AS%20%3FcurrentLocation).%0A%20%20%20%20%20%20%20%20%20%20SERVICE%20wikibase%3Aaround%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP625%20%3FotherLocation.%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20bd%3AserviceParam%20wikibase%3Acenter%20%3FcurrentLocation.%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20bd%3AserviceParam%20wikibase%3Aradius%20'{radius}'.%20%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%23%20x%20is%20named%20after%0A%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP138%20%3Fsomebody%20.%0A%0A%20%20%20%20%20%20%20%20%20%20%23%20which%20Class%20is%20x%20(e.g.%20Street%2C%20church)%0A%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP31%20%20%3Fclass%20.%20%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20x%20is%20a%20Street%0A%20%20%20%20%20%20%20%20%20%20%23%3Fx%20wdt%3AP31%20wd%3AQ79007%20.%0A%0A%20%20%20%20%20%20%20%20%20%20%23%20hat%20Staat%20Deutschland%0A%20%20%20%20%20%20%20%20%20%20%23%20%3Fx%20wdt%3AP17%20%20%3FQ183%20.%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20Retrieve%20Labels%0A%20%20%20%20%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22de%22.%20%7D%20%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20Compute%20Distance%0A%20%20%20%20%20%20%20%20%20%20%23%20BIND(geof%3Adistance(%3FotherLocation%2C%20%3FcurrentLocation)%20as%20%3Fdist)%20%0A%20%20%20%20%20%20%20%20%7D%20%0A%0A%20%20%20%20%20%20%20%20LIMIT%20{limit}%0A%20%20%20%20%20%20%20%20%23ORDER%20BY%20%3Fdist'''.format(
+    map = '''https://query.wikidata.org/embed.html#%23defaultView%3AMap%0A%20%20%20%20%20%20%20%20prefix%20bd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.bigdata.com%2Frdf%23%3E%0A%20%20%20%20%20%20%20%20prefix%20geo%3A%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0A%20%20%20%20%20%20%20%20prefix%20wd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0A%20%20%20%20%20%20%20%20prefix%20wdt%3A%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F%3E%0A%20%20%20%20%20%20%20%20prefix%20wikibase%3A%20%3Chttp%3A%2F%2Fwikiba.se%2Fontology%23%3E%0A%0A%0A%20%20%20%20%20%20%20%20SELECT%20%3FxLabel%20%3Fx%20%3FsomebodyLabel%20%3Fsomebody%20%3FotherLocation%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20(GROUP_CONCAT(%3FclassLabel%3B%20separator%3D'%2C%20')%20AS%20%3Fclassdescription)%0A%20%20%20%20%20%20%20%20WHERE%0A%20%20%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20%20BIND('Point({longitude}%20{latitude})'%5E%5Egeo%3AwktLiteral%20AS%20%3FcurrentLocation).%0A%20%20%20%20%20%20%20%20%20%20SERVICE%20wikibase%3Aaround%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP625%20%3FotherLocation.%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20bd%3AserviceParam%20wikibase%3Acenter%20%3FcurrentLocation.%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20bd%3AserviceParam%20wikibase%3Aradius%20'{radius}'.%20%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%23%20x%20is%20named%20after%0A%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP138%20%3Fsomebody%20.%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20that%20somebody%20is%20a%20human%0A%20%20%20%20%20%20%20%20%20%20%3Fsomebody%20wdt%3AP31%20wd%3AQ5%20.%0A%0A%20%20%20%20%20%20%20%20%20%20%23%20which%20Class%20is%20x%20(e.g.%20Street%2C%20church)%0A%20%20%20%20%20%20%20%20%20%20%3Fx%20wdt%3AP31%20%20%3Fclass%20.%20%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20the%20class%20has%20a%20german%20label%20%0A%20%20%20%20%20%20%20%20%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%20%20%20%20%20%20%20%20FILTER(%20lang(%3FclassLabel)%20%3D%20%22de%22%20)%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%23%20Retrieve%20Labels%0A%20%20%20%20%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22de%22.%20%7D%20%0A%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%7D%20%0A%20%20%20%20%20%20%20%20GROUP%20BY%20%3FxLabel%20%3Fx%20%3FsomebodyLabel%20%3Fsomebody%20%3FotherLocation%0A%20%20%20%20%20%20%20%20LIMIT%20{limit}%0A%20%20%20%20%20%20%20%20'''.format(
         longitude=longitude, latitude=latitude, radius=radius, limit=limit)
 
     return map
+
+
+def create_abstract_level2(somebody, somebodys_name):
+    query = '''
+            prefix owl:  <http://www.w3.org/2002/07/owl#>
+            prefix dbo:  <http://dbpedia.org/ontology>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+            SELECT ?comment # ?abstract 
+
+            WHERE {{
+                 ?s owl:sameAs <{somebody}> .
+                 ?s rdfs:comment ?comment.
+                 FILTER( lang(?comment) = "en" )
+
+                 #?s dbo:abstract ?abstract .
+                 #FILTER( lang(?abstract) = "en" )
+
+            }}
+            '''.format(somebody=somebody)
+
+    # Initializing SPARQL Wrapper and querying
+    endpoint = 'http://dbpedia.org/sparql'
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    result = sparql.query().convert()
+
+    # Trying to retrieve the comment. In case there is none or the person was not found. Return the error message.
+    try:
+        abstract = result['results']['bindings'][0]['comment']['value']
+    except:
+        abstract = 'Sorry, there is no description of {}.'.format(somebodys_name)
+
+    return abstract
+
+
+def create_df_level2(somebody, current_latitude, current_longitude):
+    query = '''
+        prefix bd:       <http://www.bigdata.com/rdf#>
+        prefix wd:       <http://www.wikidata.org/entity/>
+        prefix wdt:      <http://www.wikidata.org/prop/direct/>
+        prefix wikibase: <http://wikiba.se/ontology#>
+
+
+        SELECT ?x ?xLabel ?Location ?countryLabel
+            (GROUP_CONCAT(?classLabel; separator=', ') AS ?classdescription)
+
+        WHERE {{
+          # x is named after
+          ?x wdt:P138 <{somebody}> ;
+
+          # x has location
+             wdt:P625 ?Location ;
+
+          # which Class is x (e.g. Street, church)
+             wdt:P31  ?class . 
+
+          # the class has a german label 
+          ?class rdfs:label ?classLabel .
+          FILTER ( lang(?classLabel) = "de" )
+
+          # located in Germany, Austria, or Switzerland
+          ?x wdt:P17 ?country .
+          FILTER ( ?country = wd:Q39 || ?country = wd:Q40 || ?country = wd:Q183 )
+
+          # Retrieve Labels
+          SERVICE wikibase:label {{ bd:serviceParam wikibase:language "de". }}
+
+        }}
+        GROUP BY ?x ?xLabel ?Location ?countryLabel
+        LIMIT 1000
+
+    '''.format(somebody=somebody)
+
+    # Initializing SPARQL Wrapper and querying
+    endpoint = 'https://query.wikidata.org/sparql'
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    # Results
+    df = pd.DataFrame(results['results']['bindings'])
+    for col in df.columns:
+        df[col] = df[col].apply(lambda x: x['value'])
+
+    # compute distance
+    df['distance'] = df['Location'].apply(lambda point: compute_distance(new_point=point,
+                                                                         current_longitude=current_longitude,
+                                                                         current_latitude=current_latitude))
+
+    # Sort according to distance
+    df = df.sort_values(by=['distance'])
+
+    return df
+
+
+def create_map_level2(somebody):
+    map = '''https://query.wikidata.org/embed.html#%20%20%20%20%23defaultView%3AMap%0A%20%20%20%20prefix%20bd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.bigdata.com%2Frdf%23%3E%0A%20%20%20%20prefix%20wd%3A%20%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0A%20%20%20%20prefix%20wdt%3A%20%20%20%20%20%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F%3E%0A%20%20%20%20prefix%20wikibase%3A%20%3Chttp%3A%2F%2Fwikiba.se%2Fontology%23%3E%0A%0A%0A%20%20%20%20SELECT%20%3Fx%20%3FxLabel%20%3FLocation%20%3FcountryLabel%0A%20%20%20%20%20%20%20%20(GROUP_CONCAT(%3FclassLabel%3B%20separator%3D'%2C%20')%20AS%20%3Fclassdescription)%0A%0A%20%20%20%20WHERE%20%7B%0A%20%20%20%20%20%20%23%20x%20is%20named%20after%0A%20%20%20%20%20%20%3Fx%20wdt%3AP138%20<{somebody}>%20%3B%0A%0A%20%20%20%20%20%20%23%20x%20has%20location%0A%20%20%20%20%20%20%20%20%20wdt%3AP625%20%3FLocation%20%3B%0A%0A%20%20%20%20%20%20%23%20which%20Class%20is%20x%20(e.g.%20Street%2C%20church)%0A%20%20%20%20%20%20%20%20%20wdt%3AP31%20%20%3Fclass%20.%20%0A%0A%20%20%20%20%20%20%23%20the%20class%20has%20a%20german%20label%20%0A%20%20%20%20%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%20%20%20%20FILTER%20(%20lang(%3FclassLabel)%20%3D%20%22de%22%20)%0A%0A%20%20%20%20%20%20%23%20located%20in%20Germany%2C%20Austria%2C%20or%20Switzerland%0A%20%20%20%20%20%20%3Fx%20wdt%3AP17%20%3Fcountry%20.%0A%20%20%20%20%20%20FILTER%20(%20%3Fcountry%20%3D%20wd%3AQ39%20%7C%7C%20%3Fcountry%20%3D%20wd%3AQ40%20%7C%7C%20%3Fcountry%20%3D%20wd%3AQ183%20)%0A%0A%20%20%20%20%20%20%23%20Retrieve%20Labels%0A%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22de%22.%20%7D%0A%0A%20%20%20%20%7D%0A%20%20%20%20GROUP%20BY%20%3Fx%20%3FxLabel%20%3FLocation%20%3FcountryLabel%0A%20%20%20%20LIMIT%201000'''.format(
+        somebody=somebody)
+
+    return map
+
+
+def compute_distance(new_point, current_longitude, current_latitude):
+    new_location = np.array([float(x) for x in new_point[6:-1].split(' ')][::-1])
+    current_location = np.array([float(current_longitude), float(current_latitude)][::-1])
+    distance = hs.haversine(new_location, current_location)
+    return distance
+
+def get_somebodys_name(somebody):
+    somebodys_name = "TO DO"
+    return somebodys_name
+
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from SPARQL_queries import create_df_level1, create_map_level1
+from SPARQL_queries import create_df_level2, create_map_level2, create_abstract_level2
+from SPARQL_queries import get_somebodys_name
 
 # activate virtual env: venv\Scripts\activate.bat
 
@@ -35,7 +37,7 @@ def index_post():
                            radius=radius,
                            column_names=df.columns.values,
                            row_data=list(df.values.tolist()),
-                           link_column="somebodyLabel",
+                           link_column="somebody",
                            zip=zip,
                            _map=_map)
 
@@ -45,9 +47,69 @@ def level1():
     return render_template('level1.html')
 
 
+@app.route("/level1", methods=['POST'])
+def level1_post():
+    if "somebodyLabel" in request.form:
+        latitude = 49.49671
+        longitude = 8.47955
+        somebody = request.form['somebodyLabel']
+        somebodys_name = get_somebodys_name(somebody)
+        abstract = create_abstract_level2(somebody, somebodys_name)
+        df = create_df_level2(somebody, current_latitude=latitude, current_longitude=longitude)
+        _map = create_map_level2(somebody)
+        return render_template('level2.html',
+                               latitude=latitude,
+                               longitude=longitude,
+                               somebody=somebody,
+                               somebodys_name=somebodys_name,
+                               abstract=abstract,
+                               column_names=df.columns.values,
+                               row_data=list(df.values.tolist()),
+                               zip=zip,
+                               _map=_map)
+    else:
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        radius = request.form['radius']
+        if radius == "":
+            radius = '10'
+        df = create_df_level1(latitude=latitude, longitude=longitude, radius=radius, limit='30')
+        _map = create_map_level1(latitude=latitude, longitude=longitude, radius=radius, limit='30')
+        return render_template('level1.html',
+                               latitude=latitude,
+                               longitude=longitude,
+                               radius=radius,
+                               column_names=df.columns.values,
+                               row_data=list(df.values.tolist()),
+                               link_column="somebody",
+                               zip=zip,
+                               _map=_map)
+
+
 @app.route("/level2")
 def level2():
     return render_template('level2.html')
+
+
+@app.route("/level2", methods=['POST'])
+def level2_post():
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    somebody = request.form['somebody']
+    somebodys_name = request.form['somebodys_name']
+    abstract = create_abstract_level2(somebody, somebodys_name)
+    df = create_df_level2(somebody, current_latitude=latitude, current_longitude=longitude)
+    _map = create_map_level2(somebody)
+    return render_template('level2.html',
+                           latitude=latitude,
+                           longitude=longitude,
+                           somebody=somebody,
+                           somebodys_name=somebodys_name,
+                           abstract=abstract,
+                           column_names=df.columns.values,
+                           row_data=list(df.values.tolist()),
+                           zip=zip,
+                           _map=_map)
 
 
 if __name__ == "__main__":
